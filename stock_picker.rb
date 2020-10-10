287ee3b67c2b5c194a86158383e42edd6ceecef8
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2020-10-09 23:11:55 -0600

    rough sketch was so close to done i just finished it

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,8 +2,33 @@
 #require 'pry'; binding.pry # pry break point
 
 def stock_picker(prices)
-  return [1,4]
+	buy_day = 0
+	sell_day = 0
+	profit = 0
+	i = 0
+	j = 0
+
+	while i < prices.length
+		j = i
+		while j < prices.length
+			if prices[j] - prices[i] > profit
+				buy_day = i
+				sell_day = j
+				profit = prices[j] - prices[i]
+			end
+			j += 1
+		end
+		i += 1
+	end
+	return [buy_day, sell_day]
 end
 
 price_array = [17,3,6,9,15,8,6,1,10]
 p stock_picker(price_array)
+
+price_array = [17,10,9,7,5,3,1]
+p stock_picker(price_array)
+
+price_array = [1,17,3,6,9,15,8,6,1,10]
+p stock_picker(price_array)
+
