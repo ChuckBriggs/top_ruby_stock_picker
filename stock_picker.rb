7581d9320c4f71a3a4eebcefe1a5f4545f707f08
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2020-10-09 22:38:39 -0600

    prep work

---
+++ stock_picker.rb
@@ -0,0 +1,9 @@
+# stock_picker.rb
+#require 'pry'; binding.pry # pry break point
+
+def stock_picker(prices)
+  return [1,4]
+end
+
+price_array = [17,3,6,9,15,8,6,1,10]
+p stock_picker(price_array)
